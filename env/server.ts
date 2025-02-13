c947be58ec55728edf733d3499436b786b4cc23b
Ross Cohen <rosscohen1990@gmail.com>
2025-02-13 00:33:04 -0500

    u

--- env/server.ts
+++ env/server.ts
@@ -5,11 +5,11 @@ import { z } from 'zod'
 export const serverEnv = createEnv({
   server: {
     XAI_API_KEY: z.string().min(1),
-    AZURE_RESOURCE_NAME: z.string().min(1),
-    AZURE_API_KEY: z.string().min(1),
+    // AZURE_RESOURCE_NAME: z.string().min(1),
+    // AZURE_API_KEY: z.string().min(1),
     OPENAI_API_KEY: z.string().min(1),
     ANTHROPIC_API_KEY: z.string().min(1),
-    CEREBRAS_API_KEY: z.string().min(1),
+    // CEREBRAS_API_KEY: z.string().min(1),
     GROQ_API_KEY: z.string().min(1),
     E2B_API_KEY: z.string().min(1),
     UPSTASH_REDIS_REST_URL: z.string().min(1).url(),
@@ -24,7 +24,7 @@ export const serverEnv = createEnv({
     SANDBOX_TEMPLATE_ID: z.string().min(1),
     GOOGLE_MAPS_API_KEY: z.string().min(1),
     MAPBOX_ACCESS_TOKEN: z.string().min(1),
-    TRIPADVISOR_API_KEY: z.string().min(1),
+    // TRIPADVISOR_API_KEY: z.string().min(1),
     AVIATION_STACK_API_KEY: z.string().min(1),
     CRON_SECRET: z.string().min(1),
     BLOB_READ_WRITE_TOKEN:  z.string().min(1),
