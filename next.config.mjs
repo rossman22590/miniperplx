e20fac06b832f95857f1387a5e910835f9e831ec
Ross Cohen <rosscohen1990@gmail.com>
2025-04-18 17:51:54 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -21,10 +21,6 @@ const nextConfig = {
                         value: 'nosniff',
                     },
                     {
-                        key: 'X-Frame-Options',
-                        value: 'SAMEORIGIN',
-                    },
-                    {
                         key: 'Referrer-Policy',
                         value: 'strict-origin-when-cross-origin',
                     },
